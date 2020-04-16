8706117a3987450fa003ad34a8c66d1dae927e68
whodatboi8021 <63757979+whodatboi8021@users.noreply.github.com>
2020-04-15 17:44:57 -0700

    Update application.py

--- application/application.py
+++ application/application.py
@@ -53,13 +53,13 @@ class Application(Cog):
             )
         try:
             await ctx.author.send(
-                "Let's start right away! You have maximum of 2 minutes for each question. Are you applying for free agency?"
+                "Let's start right away! You have maximum of 2 minutes for each question.\nWhat position are you applying for?"
             )
         except discord.Forbidden:
             return await ctx.send(
                 "I don't seem to be able to DM you. Do you have closed DMs?"
             )
-        await ctx.send(f"Hello! {ctx.author.mention}, I've sent you a DM.")
+        await ctx.send(f"Okay, {ctx.author.mention}, I've sent you a DM.")
 
         def check(m):
             return m.author == ctx.author and m.channel == ctx.author.dm_channel
@@ -78,7 +78,7 @@ class Application(Cog):
             age = await self.bot.wait_for("message", timeout=120, check=check)
         except asyncio.TimeoutError:
             return await ctx.send("You took too long. Try again, please.")
-        await ctx.author.send("EU or NA?")
+        await ctx.author.send("NA or EU?")
         try:
             timezone = await self.bot.wait_for("message", timeout=120, check=check)
         except asyncio.TimeoutError:
@@ -94,7 +94,7 @@ class Application(Cog):
         except asyncio.TimeoutError:
             return await ctx.send("You took too long. Try again, please.")
         await ctx.author.send(
-            "Tell the teams a little bit about you."
+            "Tell the teams a little bit about yourself."
         )
         try:
             experience = await self.bot.wait_for("message", timeout=120, check=check)
@@ -114,21 +114,22 @@ class Application(Cog):
             f"User: {ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})"
         )
         embed.add_field(name="Name:", value=name.content, inline=True)
-        embed.add_field(name="Rank:", value=rank.content, inline=True)
-        embed.add_field(name="Region:", value=region.content, inline=True)
-        embed.add_field(name="Strenghts:", value=Strenghts.content, inline=True)
-        embed.add_field(name="Weaknesses:", value=Weaknesses.content, inline=True)
-        embed.add_field(name="About me:", value=about.content, inline=True)
+        embed.add_field(name="Age:", value=age.content, inline=True)
+        embed.add_field(name="Timezone:", value=timezone.content, inline=True)
+        embed.add_field(name="Desired position:", value=position.content, inline=True)
+        embed.add_field(name="Active days/week:", value=days.content, inline=True)
+        embed.add_field(name="Active hours/day:", value=hours.content, inline=True)
         embed.add_field(
-            name="Teams interested in:", value=teams.content, inline=False
+            name="Previous experience:", value=experience.content, inline=False
         )
+        embed.add_field(name="Reason:", value=reason.content, inline=False)
 
         await channel.send(embed=embed)
 
         await ctx.author.add_roles(role_add)
 
         await ctx.author.send(
-            "Your application has been submitted! Thank you!"
+            "Your application has been submitted. Thank you!"
         )
         self.antispam[ctx.guild][ctx.author].stamp()
 
@@ -154,7 +155,7 @@ class Application(Cog):
         if applicant is None:
             try:
                 await ctx.guild.create_role(
-                    name="FA Application", reason="Application cog setup"
+                    name="Free Agent", reason="Application cog setup"
                 )
             except discord.Forbidden:
                 return await ctx.send(
@@ -162,7 +163,7 @@ class Application(Cog):
                 )
         if channel is None:
             await ctx.send(
-                "Do you want everyone to see the applications channel? (yes/no)"
+                "Do you want everyone to see the application channel? (yes/no)"
             )
             try:
                 await self.bot.wait_for("message", timeout=30, check=pred)
@@ -184,7 +185,7 @@ class Application(Cog):
                 }
             try:
                 await ctx.guild.create_text_channel(
-                    "Applications",
+                    "free-agent-application",
                     overwrites=overwrites,
                     reason="Application cog setup",
                 )
@@ -201,8 +202,7 @@ class Application(Cog):
     @commands.guild_only()
     @checks.bot_has_permissions(manage_roles=True)
     async def accept(self, ctx: commands.Context, target: discord.Member):
-        """Accept a staff applicant.
-
+        """Accept Free Agent applicant.
         <target> can be a mention or an ID."""
         applicant = get(ctx.guild.roles, name="Free Agent")
         role = MessagePredicate.valid_role(ctx)
@@ -229,8 +229,7 @@ class Application(Cog):
     @commands.guild_only()
     @checks.bot_has_permissions(manage_roles=True)
     async def deny(self, ctx: commands.Context, target: discord.Member):
-        """Deny a staff applicant.
-
+        """Deny Free Agent applicant.
         <target> can be a mention or an ID"""
         applicant = get(ctx.guild.roles, name="Free Agent")
         if applicant in target.roles:
